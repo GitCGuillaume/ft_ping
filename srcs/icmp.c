4dc67de1b0849535a90ade2b6bce7a6a4f19be84
gchopin <gchopin@student.42.fr>
2023-06-29 18:12:36 +0200

    save

--- srcs/icmp.c
+++ srcs/icmp.c
@@ -9,6 +9,17 @@ void    icmpResponse(void) {
 
 }
 
+/* max 16bits */
+uint16_t    checksum(uint16_t *hdr, size_t len) {
+    uint16_t sum = 0;
+
+    while (len > 0) {
+        sum += *hdr++;
+        len -= sizeof(uint16_t);
+    }
+    return (~sum);
+}
+
 /*
     type + code important for internet control message protocol
     https://www.ibm.com/docs/fr/qsip/7.5?topic=applications-icmp-type-code-ids
@@ -16,18 +27,23 @@ void    icmpResponse(void) {
     tldr : you need type, then you have access to a list of code
     checksum is a error detection method
 */
+
+
 void    runIcmp(struct addrinfo *client, int fdSocket) {
     struct msghdr msgResponse;
-    struct icmphdr  icmpRequest;
+    struct icmphdr  icmp;
     struct addrinfo *mem = client;
 
     int result = -1;
-    icmpRequest.type = 8;
-    icmpRequest.code = 0;
-    icmpRequest.checksum = 0;
+    ft_memset(&icmp, 0, sizeof(struct icmphdr));
+    icmp.type = ICMP_ECHO;
+    icmp.code = 0;
+    icmp.checksum = checksum((uint16_t *)icmp, sizeof(icmp));
+    icmp.un.echo.id = getpid();
+    icmp.un.echo.sequence = htons(1);
     while (mem) {
 //        printf("%d\n", mem->ai_flags);
-        result = sendto(fdSocket, &icmpRequest,
+        result = sendto(fdSocket, &icmp,
             sizeof(struct icmphdr), 0,
             (struct sockaddr *)mem, sizeof(struct sockaddr));
         if (result < 0) {
