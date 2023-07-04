07aef2661f2ac12ded9329078bb6436b65fc676d
gchopin <gchopin@student.42.fr>
2023-07-04 19:08:13 +0200

    save du 04/07

--- srcs/icmp.c
+++ srcs/icmp.c
@@ -5,8 +5,37 @@ void    icmpRequest(void) {
 
 }
 
-void    icmpResponse(void) {
+void    bitwiseMask(void *dst, void *src, size_t nb) {
+    uint8_t mask = 0;
 
+    mask = *(uint8_t *)src << nb;
+    *(uint8_t *)dst = mask >> nb;
+    return ;
+}
+
+void    icmpResponse(struct msghdr *msg, ssize_t recvMsgRes) {
+    struct iovec *iov = msg->msg_iov;
+    struct iphdr    ip;
+    struct icmphdr  icmp;
+    char *buff = iov->iov_base;
+    /*printf("res: %ld\n", recvMsgRes);
+    for (int i = 0; i < 84; ++i) {
+        printf("%02x ", buff[i]);
+    }
+    ipHeader.ihl = buff[0];
+    ipHeader.version = buff[1];
+    printf("\nsizeof ipH: %lu\n", sizeof(ipHeader));
+    printf("\nihl: %u\nver: %u\n", ipHeader.ihl, ipHeader.version);*/
+    ft_memset(&ip, 0, sizeof(ip));
+    ft_memset(&icmp, 0, sizeof(icmp));
+    bitwiseMask(&ip, buff, sizeof(uint8_t));//tests ihl
+    bitwiseMask(&ip, buff + sizeof(uint8_t), sizeof(uint8_t));//tests ihl
+    //bitwiseMask(&ip + sizeof(uint16_t), buff + sizeof(uint16_t), sizeof(uint16_t));//tests ihl
+    //ft_memcpy(&ip, buff, 1);
+    //ft_memcpy(&icmp, buff + 20, sizeof(icmp));
+    printf("ihl: %u ver: %u tos: %u tot_len: %u id: %u\nf_off: %u ttl: %u protocol: %u\n check: %u saddr: %u daddr: %u\n"
+        , ip.ihl, ip.version, ip.tos, ip.tot_len, ip.id, ip.frag_off, ip.ttl, ip.protocol, ip.check, ip.saddr, ip.daddr);
+    printf("type: %u code: %u\nchecksum: %u id: %u, seq: %u\n", icmp.type, icmp.code, icmp.checksum, icmp.un.echo.id, icmp.un.echo.sequence);
 }
 
 /* max 16bits */
@@ -31,32 +60,6 @@ uint16_t    checksum(uint16_t *hdr, size_t len) {
     checksum is a error detection method
 */
 
-void printBits(unsigned int num)
-{
-   for(int bit=0;bit<(sizeof(unsigned int) * 8); bit++)
-   {
-      printf("%i ", num & 0x01);
-      num = num >> 1;
-   }
-}
-
-void    show(struct icmphdr *icmp) {
-    printf("ap b\n");
-    if (!icmp)
-        return ;
-    printf("ap s\n");
-    printBits((unsigned int)icmp->type);
-    printf("type: %u\ncode: %u\nchecksum: %u\nid: %u\nseq: %u\n",
-        icmp->type, icmp->code, icmp->checksum, icmp->un.echo.id, icmp->un.echo.sequence);
-   //printf("type: %u\ncode: %u\n",
-     //   icmp->type, 0);
-    
-}
-
-void    show2(struct cmsghdr *truc) {
-    printf("cmsg_len: %ld\ncmsg_level: %d\ncmsg_type: %d\n", truc->cmsg_len, truc->cmsg_level, truc->cmsg_type);
-}
-
 /* (ipv4 max)65535 - (sizeof ip)20 - (sizeof icmp)8 */
 void    runIcmp(struct addrinfo *client, int fdSocket) {
     struct msghdr msgResponse;
@@ -65,7 +68,7 @@ void    runIcmp(struct addrinfo *client, int fdSocket) {
     struct icmphdr  icmp;
     struct addrinfo *mem = client;
     struct iovec msg[1];
-    
+    ssize_t recvMsgRes = -1;
     //struct cmsghdr cmsg;
     ft_memset(buff, 0, 65507);
     ft_memset(buff2, 0, 65507);
@@ -77,7 +80,7 @@ void    runIcmp(struct addrinfo *client, int fdSocket) {
     icmp.type = ICMP_ECHO;
     icmp.code = 0;
     icmp.un.echo.id = getpid();
-    int i = 0;
+    int i = 1;
     while (1) {
         //while (mem) {
             icmp.un.echo.sequence = htons(i);
@@ -103,19 +106,16 @@ void    runIcmp(struct addrinfo *client, int fdSocket) {
             msgResponse.msg_iovlen = 1;
             msgResponse.msg_control = 0;
             msgResponse.msg_controllen = 0;
-            //show((struct icmphdr *)msgResponse.msg_iov->iov_base);
-            //show2(msgResponse.msg_control);
             printf("msgname: %p\nmsg_len: %u\nmsg_iov: %p\n iovlen:%lu\n msg_control: %p\n msg_controllen: %lu\n fl: %d\n",
                 msgResponse.msg_name,
                 msgResponse.msg_namelen,
                 msgResponse.msg_iov, msgResponse.msg_iovlen,
                 msgResponse.msg_control, msgResponse.msg_controllen,
                 msgResponse.msg_flags);
-            printf("res: %lu\n", recvmsg(fdSocket, &msgResponse, MSG_WAITALL));
-            //mhdr = CMSG_FIRSTHDR;
+            printf("res: %lu\n", (recvMsgRes = recvmsg(fdSocket, &msgResponse, MSG_WAITALL)));
+            
+            icmpResponse(&msgResponse, recvMsgRes);
             printf("ok\n");
-            show((struct icmphdr *)msgResponse.msg_iov->iov_base);
-            //show2(msgResponse.msg_control);
             printf("msgname: %p\nmsg_len: %u\nmsg_iov: %p\n iovlen:%lu\n msg_control: %p\n msg_controllen: %lu\n fl: %d\n",
                 msgResponse.msg_name,
                 msgResponse.msg_namelen,
