d9f5470ea6d0122ab4e500a8811059b92a43aabf
gchopin <gchopin@student.42.fr>
2023-07-05 19:06:22 +0200

    avancement écriture sur struct ip

--- srcs/icmp.c
+++ srcs/icmp.c
@@ -5,12 +5,38 @@ void    icmpRequest(void) {
 
 }
 
-void    bitwiseMask(void *dst, void *src, size_t nb) {
-    uint8_t mask = 0;
+void printBits(unsigned int num)
+{
+   for(unsigned int bit=0;bit< 256; bit++)
+   {
+      printf("%i ", num & 0x01);
+      num = num >> 1;
+   }
+   printf("\n");
+}
 
-    mask = *(uint8_t *)src << nb;
-    *(uint8_t *)dst = mask >> nb;
+/*
+void    bitwiseMask(void *dst, unsigned char *src, unsigned int hexa, size_t nb) {
+    unsigned int mask = *src >> nb;
+    printBits(mask);
+    unsigned int    copyBits = *src;
+    unsigned int    mask = (*src << nb) & hexa;
+
+    printf("mask: %u\n", mask);
+    printBits(mask);
+    *dst = (mask >> (nb - 1));
+
+    //printBits((unsigned int)mask >> (nb-1));
     return ;
+}
+*/
+
+void    bigBitMask(char *buff, int jump, int nb, uint32_t *store) {
+    uint32_t    mask = *store;
+
+    *store = *(buff + jump) << nb;
+    //if (mask != 0)
+    //    *store |= mask;
 }
 
 void    icmpResponse(struct msghdr *msg, ssize_t recvMsgRes) {
@@ -28,11 +54,43 @@ void    icmpResponse(struct msghdr *msg, ssize_t recvMsgRes) {
     printf("\nihl: %u\nver: %u\n", ipHeader.ihl, ipHeader.version);*/
     ft_memset(&ip, 0, sizeof(ip));
     ft_memset(&icmp, 0, sizeof(icmp));
-    bitwiseMask(&ip, buff, sizeof(uint8_t));//tests ihl
-    bitwiseMask(&ip, buff + sizeof(uint8_t), sizeof(uint8_t));//tests ihl
+    //bitwiseMask(&ip, (unsigned char *)*buff >> 16, 0xF, 0);//tests ihl
+   // bitwiseMask(&ip, (unsigned char *)buff, 0xF0, 4);
+    //bitwiseMask(&ip, buff + sizeof(uint8_t), sizeof(uint8_t));//tests ihl
     //bitwiseMask(&ip + sizeof(uint16_t), buff + sizeof(uint16_t), sizeof(uint16_t));//tests ihl
-    //ft_memcpy(&ip, buff, 1);
-    //ft_memcpy(&icmp, buff + 20, sizeof(icmp));
+    //ft_memcpy(&ip, buff, sizeof(ip));
+    //printBits(*buff);
+    /*for (int i = 0; i < 32 * 8; ++i) {
+        printf("%02x", ((unsigned char *)buff)[i]);
+    }*/
+    printf("\n");
+    ip.ihl = *buff & 0xF;
+    *buff = *buff >> 4;
+    ip.version = *buff;//(*buff & 0xF0) >> 4;
+    ++buff;
+    ip.tos = *buff;// >> 16;
+    //printBits(*buff);
+    ++buff;
+    /*for (int i = 0; i < 32 * 8; ++i) {
+        printf("%02x", ((unsigned char *)buff)[i]);
+    }*/
+    ip.tot_len = (*buff << 8) | *(buff + 1);
+    buff += 2;
+    ip.id = (*buff << 8) | *(buff + 1);
+    buff += 2;
+    ip.frag_off = *buff << 8 | *(buff + 1);
+    buff += 2;
+    ip.ttl = *buff;
+    ++buff;
+    ip.protocol = *buff;
+    ++buff;
+    ip.check = *buff << 8 | *(buff + 1);
+    buff += 2;
+    //ip.saddr = *buff | *(buff + 1) << 8;// | *(buff + 2) << 8 | *(buff + 3);
+    bigBitMask(buff, 0, 24, &ip.saddr);
+    bigBitMask(buff, 1, 16, &ip.saddr);
+    printBits(ip.saddr);
+    printf("\n");
     printf("ihl: %u ver: %u tos: %u tot_len: %u id: %u\nf_off: %u ttl: %u protocol: %u\n check: %u saddr: %u daddr: %u\n"
         , ip.ihl, ip.version, ip.tos, ip.tot_len, ip.id, ip.frag_off, ip.ttl, ip.protocol, ip.check, ip.saddr, ip.daddr);
     printf("type: %u code: %u\nchecksum: %u id: %u, seq: %u\n", icmp.type, icmp.code, icmp.checksum, icmp.un.echo.id, icmp.un.echo.sequence);
