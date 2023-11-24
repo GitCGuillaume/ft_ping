d0ec2165ee70fad54a4db4c575ef9bf865bdf229
gchopin <gchopin@student.42.fr>
2023-11-24 19:25:57 +0100

    timeur

--- bonus/srcs/icmpRequest_bonus.c
+++ bonus/srcs/icmpRequest_bonus.c
@@ -2,8 +2,9 @@
 #include "tools_bonus.h"
 
 /* Get request response */
-static int    icmpGetResponse() {
+static int    icmpGetResponse(struct timeval *elapsedStartTime) {
     char buff[ECHO_REPLY_SIZE];
+    struct timeval elapsedEndTime;
     struct msghdr msgResponse;
     struct iovec msg[1];
     struct timeval tvA;
@@ -16,7 +17,90 @@ static int    icmpGetResponse() {
     msgResponse.msg_iov = msg;
     msgResponse.msg_iovlen = 1;
     int cpyErrno = errno;
-
+//sleep(3);
+    if (gettimeofday(&elapsedEndTime, 0) < 0) {
+        exitInet();
+    }
+    
+    //now need to correct elapsed time
+    //printf("%f\n", (elapsedEndTime.tv_sec - elapsedStartTime->tv_sec) * 1000.0f
+    //    + (elapsedEndTime.tv_usec - elapsedStartTime->tv_usec) / 1000.0f);
+    float it_usec = t_flags.interval - (long)t_flags.interval;
+    float it_sec = (long)t_flags.interval;
+    long    tv_sec = elapsedEndTime.tv_sec - elapsedStartTime->tv_sec;
+    long    tv_usec = elapsedEndTime.tv_usec - elapsedStartTime->tv_usec;
+    //printf("it_usec * 1000000: %ld\n", (long)(it_usec * 1000000));
+    tv_usec *= 1000;
+    long convertUsec = (long)(it_usec * 1000000);
+    //printf("tv_usec: %ld\n", tv_usec);
+    //printf("conv:%ld\n", convertUsec);
+    if (tv_usec < convertUsec) {
+        (void)tv_sec;(void)it_sec;
+        long milli = convertUsec - tv_usec;
+        //printf("milli:%ld\n", milli);
+        convertUsec -= milli;
+        //tv_sec -= 1;
+        //printf("tv_sec:%ld\n", tv_sec);
+        elapsedEndTime.tv_sec = tv_sec;
+        elapsedEndTime.tv_usec = milli;
+        if (setsockopt(fdSocket, SOL_SOCKET, SO_RCVTIMEO, &elapsedEndTime, sizeof(elapsedEndTime)) != 0) {
+            dprintf(2, "%s", "Couldn't set option RCVTIMEO socket.\n");
+            exitInet();
+        }
+        //printf("elapsedEndTime.tv_sec: %ld\nelapsedEndTime.tv_usec: %ld\n",
+        //    elapsedEndTime.tv_sec, elapsedEndTime.tv_usec);
+    }
+    //float time = it_sec - ((elapsedEndTime.tv_sec - elapsedStartTime->tv_sec)) +
+    //     it_usec - ((elapsedEndTime.tv_usec - elapsedStartTime->tv_usec));// / 1000000.0f);
+    //float time = it_sec - ((elapsedEndTime.tv_sec - elapsedStartTime->tv_sec) * 1000.0f) +
+    //     it_usec - ((elapsedEndTime.tv_usec - elapsedStartTime->tv_usec) / 1000.0f);
+//sleep(3);
+    /*printf("it_sec: %f it_usec: %f\n", it_sec, it_usec);
+    printf("%f\n", (elapsedEndTime.tv_sec - elapsedStartTime->tv_sec) * 1000.0f
+        + (elapsedEndTime.tv_usec - elapsedStartTime->tv_usec) / 1000.0f);
+    printf("time: %f\n", time);
+    *///time = it_sec - ((elapsedEndTime.tv_sec - elapsedStartTime->tv_sec) * 1000.0f) +
+      //   it_usec - ((elapsedEndTime.tv_usec - elapsedStartTime->tv_usec) / 1000.0f);
+    //printf("time: %f\n", time);
+   // printf("t:%ld\n", (long)(time * 1000000));
+    /*printf("elapsedEndTime.tv_sec: %ld\nelapsedEndTime.tv_usec: %ld\n",
+        elapsedEndTime.tv_sec, elapsedEndTime.tv_usec);
+    printf("AAelapsedEndTime.tv_sec - elapsedStartTime->tv_sec: %ld\n",
+        (long)it_sec - (elapsedEndTime.tv_sec - elapsedStartTime->tv_sec));
+    printf("BBelapsedEndTime.tv_usec - elapsedStartTime->tv_usec: %ld\n",
+        (long)it_usec - (elapsedEndTime.tv_usec - elapsedStartTime->tv_usec));
+    */
+    /*if ((long)(time * 1000000) > 0) {
+        elapsedEndTime.tv_sec = (long)time;
+        elapsedEndTime.tv_usec = (time - (long)time) * 1000000;
+    } else {
+        elapsedEndTime.tv_sec = 0;
+        elapsedEndTime.tv_usec = 1;
+    }*/
+    //elapsedEndTime.tv_sec
+    //    = (long)it_sec - (elapsedEndTime.tv_sec - elapsedStartTime->tv_sec);
+    //elapsedEndTime.tv_usec = (long)it_usec - (elapsedEndTime.tv_usec - elapsedStartTime->tv_usec);
+    /*while (elapsedEndTime.tv_usec < 0) {
+        elapsedEndTime.tv_sec -= 1;
+        elapsedEndTime.tv_usec += 1000000;
+    }
+    if (elapsedEndTime.tv_sec < 0)
+        elapsedEndTime.tv_sec = 0;
+    while (elapsedEndTime.tv_usec > 999999) {
+        elapsedEndTime.tv_sec += 1;
+        elapsedEndTime.tv_usec -= 1000000;
+    }*/
+    //if (elapsedEndTime.tv_usec < 0)
+    //    elapsedEndTime.tv_usec = 0;
+    
+    
+    
+//    printf("%f\n\n", (float)(elapsedEndTime.tv_sec)
+  //      + (float)(elapsedEndTime.tv_usec)/1000000.0);
+    /*if (setsockopt(fdSocket, SOL_SOCKET, SO_RCVTIMEO, &elapsedEndTime, sizeof(elapsedEndTime)) != 0) {
+        dprintf(2, "%s", "Couldn't set option RCVTIMEO socket.\n");
+        exitInet();
+    }*/
     result = recvmsg(fdSocket, &msgResponse, 0);
     if (result < 0
         && errno != EWOULDBLOCK && errno != EAGAIN && errno != EINTR) {
@@ -25,6 +109,7 @@ static int    icmpGetResponse() {
         exitInet();
     }
     if (end || result == -1){
+        printf("res:%d\n", result);
         t_flags.preload = 0;
         return (TRUE);
     }
@@ -105,6 +190,7 @@ void    runIcmp() {
         exitInet();
     struct timeval gTimer;
     struct timeval tvB;
+    struct timeval elapsedStartTime;
     char buff[ECHO_REQUEST_SIZE];
     int result = -1;
     int cpyI;
@@ -144,10 +230,16 @@ void    runIcmp() {
                     close(fdSocket);
                 exit(1);
             }
+            if (gettimeofday(&elapsedStartTime, 0) < 0) {
+                exitInet();
+            }
         }
         int interrupt = FALSE;
         while (!end && !interrupt) {
-            interrupt = icmpGetResponse();
+            interrupt = icmpGetResponse(&elapsedStartTime);
+            if (gettimeofday(&elapsedStartTime, 0) < 0) {
+                exitInet();
+            }
         }
     }
     if (end != TRUE) {
