c290e001f4581bfaaa0f912c9776b8bc6f0a4f64
gchopin <gchopin@student.42.fr>
2023-12-12 15:04:24 +0100

    update test.c

--- test.c
+++ test.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 double   ftSqrt(double num) {
-    double x = num;//(num + num / num) / 2;
-    double old = 0;
+    //x is whatever value near square root
+    double x = num;
+    double old = 0.0;
 
     if (num < 0 || num == 0)
-        return (0.0d);
+        return (0.0);
     while (x != old){
         old = x;
-        x = (x + num / x) / 2;
+        x = (x + num / x) / 2.0;
     }
     return (x);
 }
 
 
 int	main(void) {
-	double arr[] = {0.580, 1.012, 0.339};
+	double arr[] = {29.196, 10.998, 10.524};
 	double sum = 0.000000;
 	double squareSum = 0.000000;
 	double stdDev;
@@ -27,6 +28,7 @@ int	main(void) {
 	}
 	average = sum / 3;
 	stdDev = ftSqrt((squareSum / 3) - (average * average));
+	printf("d:%.90f\n", 2.0);
 	printf("%.3f %.3f %.3f\n", sum, squareSum, stdDev);
 	return (0);
 }
